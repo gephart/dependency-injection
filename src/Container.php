94f5ac531b8e8fd38d7f3a5317e54438b68b7d27
Michal Katuščák <michal@katuscak.cz>
2017-10-08 21:27:03 +0200

    Method Container::register for registering class without initialise in DI Container

--- src/Container.php
+++ src/Container.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace Gephart\DependencyInjection;
-
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 /**
  * Dependency injection container
@@ -42,7 +42,7 @@ final class Container implements ContainerInterface
                 $dependencies = $this->getDependencies($class_name);
                 $this->objects[$class_name] = new $class_name(...$dependencies);
             } catch (NotFoundException $e) {
-                throw new ContainerException("Container could not initialize '$class_name', dependencies not founds.");
+                throw new ContainerException("Container could not initialize '$class_name' because dependencies not founds.");
             } catch (\Exception $exception) {
                 throw $exception;
             }
@@ -66,13 +66,36 @@ final class Container implements ContainerInterface
     }
 
     /**
+     * @since 0.5
+     *
+     * @param $object
+     * @param string|null $class_name
+     * @return Container
+     */
+    public function register($object, string $class_name = null): Container
+    {
+        if (!$class_name) {
+            $class_name = get_class($object);
+        }
+
+        if (!is_object($object) || !$object instanceof $class_name) {
+            throw new \InvalidArgumentException("Parameter \$object must be instance of object " . $class_name);
+        }
+
+        $this->objects[$class_name] = $object;
+
+        return $this;
+    }
+
+    /**
+     * @since 0.5 ContainerException when not found class by parameter of reflection
      * @since 0.4 Now throw ContainerException
      * @since 0.2
      *
      * @param string $class_name
      * @param string $method_name
      * @return array
-     * @throws \Exception
+     * @throws ContainerException
      */
     private function getDependencies(string $class_name, string $method_name = "__construct"): array
     {
@@ -86,8 +109,14 @@ final class Container implements ContainerInterface
 
         $parameters = $reflection_class->getMethod($method_name)->getParameters();
         foreach ($parameters as $parameter) {
-            if ($parameter->getClass()) {
-                $dependencies[] = $this->get($parameter->getClass()->name);
+            try {
+                $class = $parameter->getClass();
+            } catch (\Exception $exception) {
+                throw new ContainerException("Class not found in $class_name::$method_name. " . $exception->getMessage());
+            }
+
+            if ($class) {
+                $dependencies[] = $this->get($class->name);
             } else {
                 throw new ContainerException("All parameters of $class_name::$method_name must be a class.");
             }
